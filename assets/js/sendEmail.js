53aa82e8fcc6072d2e7212acc6e1e81bdf7956aa
parhan0583 <farhan.43554@gmail.com>
2023-12-15 16:31:28 +0700

    first commit

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+emailjs.init("O8DsAx6aTXozVsKis"); // Gantilah dengan User ID Anda
+
+function sendEmail() {
+  emailjs.sendForm("service_hqcuiwo", "template_grivoct", "#contactForm")
+    .then(
+      function(response) {
+        console.log("Email sent successfully:", response);
+        alert('berhasil')
+      },
+      function(error) {
+        console.log("Failed to send email:", error);
+        alert('gagal')
+      }
+    );
+}
